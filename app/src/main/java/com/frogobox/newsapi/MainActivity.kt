0b88b72c01466fdfd9625d5a9bd52b7b6b6703f6
amirisback <faisalamircs@gmail.com>
2020-05-13 19:29:57 +0700

    update: build.gradle refactoring code

--- app/src/main/java/com/frogobox/newsapi/MainActivity.kt
+++ app/src/main/java/com/frogobox/newsapi/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.frogobox.newsapi
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.frogobox.frogonewsapi.ConsumeNewsApi
 import com.frogobox.frogonewsapi.callback.NewsResultCallback
+import com.frogobox.frogonewsapi.data.model.Article
 import com.frogobox.frogonewsapi.data.response.ArticleResponse
 import com.frogobox.frogonewsapi.util.NewsConstant.CATEGORY_HEALTH
 import com.frogobox.frogonewsapi.util.NewsConstant.COUNTRY_ID
 import com.frogobox.frogonewsapi.util.NewsUrl
+import com.frogobox.recycler.boilerplate.viewrclass.FrogoViewAdapterCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNewsApi()
+    }
 
+    private fun setupNewsApi() {
         val consumeNewsApi = ConsumeNewsApi(NewsUrl.NEWS_API_KEY) // Your API_KEY
         consumeNewsApi.usingChuckInterceptor(this)
         consumeNewsApi.getTopHeadline(
@@ -27,6 +37,7 @@ class MainActivity : AppCompatActivity() {
             object : NewsResultCallback<ArticleResponse> {
                 override fun getResultData(data: ArticleResponse) {
                     // Your Ui or data
+                    data.articles?.let { setupFrogoRecyclerView(it) }
                 }
 
                 override fun failedResult(statusCode: Int, errorMessage: String?) {
@@ -35,14 +46,45 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onShowProgress() {
                     // Your Progress Show
+                    runOnUiThread {
+                        progressView.visibility = View.VISIBLE
+                    }
                 }
 
                 override fun onHideProgress() {
                     // Your Progress Hide
+                    runOnUiThread {
+                        progressView.visibility = View.GONE
+                    }
                 }
 
             })
-
-
     }
+
+    private fun setupFrogoRecyclerView(data: List<Article>) {
+        frogorecyclerview.injector<Article>()
+            .addData(data)
+            .addCustomView(R.layout.frogo_rv_list_type_6)
+            .addEmptyView(null)
+            .addCallback(object : FrogoViewAdapterCallback<Article> {
+                override fun onItemClicked(data: Article) {}
+
+                override fun onItemLongClicked(data: Article) {}
+
+                override fun setupInitComponent(view: View, data: Article) {
+                    val tvTitle = view.findViewById<TextView>(R.id.frogo_rv_type_6_tv_title)
+                    val tvSubtitle = view.findViewById<TextView>(R.id.frogo_rv_type_6_tv_subtitle)
+                    val tvDesc = view.findViewById<TextView>(R.id.frogo_rv_type_6_tv_description)
+                    val ivPoster = view.findViewById<ImageView>(R.id.frogo_rv_type_6_iv_poster)
+
+                    tvTitle.text = data.title ?: "No Data"
+                    tvSubtitle.text = data.author ?: "No Data"
+                    tvDesc.text = data.description
+                    Glide.with(view.context).load(data.urlToImage).into(ivPoster)
+                }
+            })
+            .createLayoutLinearVertical(false)
+            .build()
+    }
+
 }
