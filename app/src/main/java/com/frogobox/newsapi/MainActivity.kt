6a05bbc8be42623f97386063d65c954fc768f63d
faisalamir <faisalamircs@gmail.com>
2021-05-10 02:28:12 +0700

    update: build.gradle remove: Kotlin-Android-Extension

--- app/src/main/java/com/frogobox/newsapi/MainActivity.kt
+++ app/src/main/java/com/frogobox/newsapi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.frogobox.newsapi
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,56 +14,61 @@ import com.frogobox.frogonewsapi.data.response.ArticleResponse
 import com.frogobox.frogonewsapi.util.NewsConstant.CATEGORY_HEALTH
 import com.frogobox.frogonewsapi.util.NewsConstant.COUNTRY_ID
 import com.frogobox.frogonewsapi.util.NewsUrl
+import com.frogobox.newsapi.databinding.ActivityMainBinding
 import com.frogobox.recycler.core.IFrogoViewAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
         setupNewsApi()
     }
 
     private fun setupNewsApi() {
         val consumeNewsApi = ConsumeNewsApi(NewsUrl.NEWS_API_KEY) // Your API_KEY
         consumeNewsApi.usingChuckInterceptor(this)
-        consumeNewsApi.getTopHeadline(
-            null,
-            null,
-            CATEGORY_HEALTH,
-            COUNTRY_ID,
-            null,
-            null,
-            object : NewsResultCallback<ArticleResponse> {
-                override fun getResultData(data: ArticleResponse) {
-                    // Your Ui or data
-                    data.articles?.let { setupFrogoRecyclerView(it) }
-                }
-
-                override fun failedResult(statusCode: Int, errorMessage: String?) {
-                    // Your failed to do
-                }
-
-                override fun onShowProgress() {
-                    // Your Progress Show
-                    runOnUiThread {
-                        progressView.visibility = View.VISIBLE
+        binding.apply {
+            consumeNewsApi.getTopHeadline(
+                null,
+                null,
+                CATEGORY_HEALTH,
+                COUNTRY_ID,
+                null,
+                null,
+                object : NewsResultCallback<ArticleResponse> {
+                    override fun getResultData(data: ArticleResponse) {
+                        // Your Ui or data
+                        data.articles?.let { setupFrogoRecyclerView(it) }
                     }
-                }
 
-                override fun onHideProgress() {
-                    // Your Progress Hide
-                    runOnUiThread {
-                        progressView.visibility = View.GONE
+                    override fun failedResult(statusCode: Int, errorMessage: String?) {
+                        // Your failed to do
                     }
-                }
 
-            })
+                    override fun onShowProgress() {
+                        // Your Progress Show
+                        runOnUiThread {
+                            progressView.visibility = View.VISIBLE
+                        }
+                    }
+
+                    override fun onHideProgress() {
+                        // Your Progress Hide
+                        runOnUiThread {
+                            progressView.visibility = View.GONE
+                        }
+                    }
+
+                })
+        }
     }
 
     private fun setupFrogoRecyclerView(data: List<Article>) {
-        frogorecyclerview.injector<Article>()
+        binding.frogorecyclerview.injector<Article>()
             .addData(data)
             .addCustomView(R.layout.frogo_rv_list_type_6)
             .addEmptyView(null)
